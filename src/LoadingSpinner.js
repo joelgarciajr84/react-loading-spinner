4017841c4b072e3c0d2575f4b0c705c8172a97bd
Joel Garcia Jr <joel.garciajr84@gmail.com>
2019-09-13 15:33:33 -0300

    First commit :tada:

---
+++ src/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LoadingSpinner extends Component {
+  render(){
+    return (
+      <div className="loading-spinner"></div>
+    )
+  }
+ 
+}
+
+export default LoadingSpinner;
